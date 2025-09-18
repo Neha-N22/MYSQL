c790f9a3478dce261acaed6dd16d44c9721a91c3
Neha N <neha22032002@gmail.com>
2025-09-18 15:31:03 +0530

    Add new customer entry for Allison in New York

--- day1.sql
+++ day1.sql
@@ -1,4 +1,6 @@
 INSERT INTO customers VALUES ('8','William','Tokyo','4006','Japan');
 
+INSERT INTO Customers (CustomerName, City, Country)
+VALUES ('Allison', 'New York', 'US');
 SELECT * FROM Customers
 WHERE CustomerID=1;
