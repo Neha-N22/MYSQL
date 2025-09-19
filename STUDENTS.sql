9ec274dd3e654a1a186ce9cdd22971222ba19af0
Neha N <neha22032002@gmail.com>
2025-09-19 14:55:03 +0530

    Add student records to the students table

--- STUDENTS.sql
+++ STUDENTS.sql
@@ -4,3 +4,9 @@ CREATE TABLE students (
     age INT,
     department VARCHAR(50),
     garde INT);
+
+    INSERT INTO students VALUES('1','Neha','23','ECE','70');
+INSERT INTO students VALUES('2','Anjana','23','EEE','90');
+INSERT INTO students VALUES('3','Amal','27','CSE','80');
+INSERT INTO students VALUES('4','Arun','25','ECE','80');
+
